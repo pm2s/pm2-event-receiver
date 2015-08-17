b4653ba9c838434a522bb94011c714a014e4cc05
s0ph1e <sonya1912@gmail.com>
2015-08-17 16:55:30 +0300

    Restart rabbitmq after enabling management tool

--- setup.sh
+++ setup.sh
@@ -8,3 +8,4 @@ rabbitmq-plugins enable rabbitmq_management
 rabbitmqctl add_user test test
 rabbitmqctl set_user_tags test administrator
 rabbitmqctl set_permissions -p / test ".*" ".*" ".*"
+service rabbitmq-server restart
